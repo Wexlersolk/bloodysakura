faf932d7ee8eb5286730951af4f3d65df1f531d4
Wexler763 <danylkarp@gmail.com>
2024-09-15 15:02:46 +0300

    starting managers

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"github.com/anthdm/hollywood/actor"
+	"log"
+	"log/slog"
+	"time"
+)
 
+type VisitRequest struct {
+	links []string
+}
+
+type Orchestrator struct {
+}
+
+func NewOrchestrator() actor.Producer {
+	return func() actor.Receiver {
+		return &Orchestrator{}
+	}
+}
+func (m *Orchestrator) handleVisitRequest(msg VisitRequest) error {
+	for _, link := range msg.links {
+		slog.Info("visiting url", "url", link)
+	}
+	return nil
+}
+func (m *Orchestrator) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case VisitRequest:
+		m.handleVisitRequest(msg)
+	case actor.Started:
+		slog.Info("Manager started")
+	case actor.Stopped:
+	}
+}
 func main() {
-	fmt.Println("Hello, World!")
+	e, err := actor.NewEngine(actor.NewEngineConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	e.Spawn(NewOrchestrator(), "Orchestrator")
+
+	time.Sleep(time.Second * 10)
 }
